f48404646596158e363806ff5400609261291486
Borisbell <jmorrison@bk.ru>
2022-05-28 13:22:15 +0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a120a5f07d9ff88815da728f95db1d38.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c9fdce889fc9501024f",
+    "url": "/static/css/main.c2dbe2a7.chunk.css"
+  },
+  {
+    "revision": "6c9fdce889fc9501024f",
+    "url": "/static/js/main.6c9fdce8.chunk.js"
+  },
+  {
+    "revision": "d1e0808e20fb2c18b4f9",
+    "url": "/static/js/1.d1e0808e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7baba3c1428ee943536a8022d42f0985",
+    "url": "/static/media/logo.7baba3c1.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/success.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/error.4072126c.svg"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/edit-btn-icon.6bfdb354.svg"
+  },
+  {
+    "revision": "dae3921355ceba50d5edfae98f4170bd",
+    "url": "/static/media/spinner.dae39213.gif"
+  },
+  {
+    "revision": "b914062f3dd12a6e171651e11de418c9",
+    "url": "/static/media/ava.b914062f.jpg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "302bdf76b9210a2a438f500909a8d36c",
+    "url": "/static/media/like-btn__active.302bdf76.svg"
+  },
+  {
+    "revision": "cd42fe98127bee976095190bc5c9877e",
+    "url": "/static/media/delete.cd42fe98.svg"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "cb0f2bbfd915a7bfa586c3a3bd2efc48",
+    "url": "/static/media/close-icon.cb0f2bbf.svg"
+  },
+  {
+    "revision": "00b1c22a19c7a1e64f19b1fbacc3a301",
+    "url": "/static/media/like-btn.00b1c22a.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/add-btn-icon.b2fce4ac.svg"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "e8ed637ab786a0f91a28c3bd509887ba",
+    "url": "/index.html"
+  }
+];
